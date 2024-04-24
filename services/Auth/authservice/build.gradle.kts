d31d8959bc91587e01e7ea9f7e03ebce0606609d
Eurico Martins <euricomartinss@gmail.com>
2024-04-25 00:54:17 +0100

    [i5] Session on a in-memory db
    - Created auth spring boot app;
    - Updated docker compose file;

---
+++ services/Auth/authservice/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.dev"
+version = "0.0.1-DEV"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-cache")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
